0843e1d75803bdad2a3fdcab3c30168cf0ca3253
Lex Houf <lexhoufio@gmail.com>
2018-03-10 08:45:50 +0100

    Improve markdown renderer

--- frontend/src/renderers/text.tsx
+++ frontend/src/renderers/text.tsx
@@ -4,13 +4,36 @@ import * as ReactMarkdown from 'react-markdown';
 
 import { createRenderer } from '../utils/createRenderer';
 import { RendererProps } from '../types';
+import { Heading } from '../components/Heading';
+import { PHONE } from '../constants';
 
 export const text = createRenderer(class extends Component<RendererProps<'text'>> {
   public render() {
-    const {data: {text}} = this.props;
+    const { data: { text } } = this.props;
 
     return (
-      <ReactMarkdown source={text} skipHtml={true}/>
+      <ReactMarkdown
+        source={text}
+        allowedTypes={[
+          'root',
+          'paragraph',
+          'strong',
+          'link',
+          'image',
+          'list',
+          'listItem',
+          'heading'
+        ]}
+        renderers={{
+          heading: ({ children, level }) => {
+            const type = level === 1 ? 'bold' : level === 2 ? 'thin' : 'subtle';
+
+            return (
+              <Heading text={children} breakpoints={{ [PHONE]: type }}/>
+            );
+          }
+        }}
+      />
     );
   }
 });
